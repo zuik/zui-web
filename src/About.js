de445df15858723c73fc331e669625d77a52030e
Duy Nguyen <zuikng@gmail.com>
2017-05-08 12:58:50 -0400

    Edit some color

--- src/About.js
+++ src/About.js
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
-import './About.css';
+import React, {Component} from 'react';
 
 class About extends Component {
-  render() {
-    return (<div className="content">
-    <p>Content content content blah blah blah blahContent content content blah blah blah blahContent content content blah blah blah blahContent content content blah blah blah blahContent content content blah blah blah blah</p>
-    </div>);
-  }
+    render() {
+        return (<div className="content">
+            <p>Content content content blah blah blah blah
+                Content content content blah blah blah blah
+                Content content content blah blah blah blah
+                Content content content blah blah blah blah
+                Content content content blah blah blah blah</p>
+        </div>);
+    }
 }
 
 export default About;
