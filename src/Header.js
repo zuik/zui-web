e9394d18cc89325826ffa7f95446f5d94ca37d3a
Duy Nguyen <zuikng@gmail.com>
2017-05-08 12:25:03 -0400

    Little prettier

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 
-class Header extends Component {
+/*class Header extends Component {
   render() {
     return (
       <div className="header">
@@ -21,6 +21,24 @@ class Navigation extends Component {
         <li><Link to={"/projects/"}>Projects</Link></li>
         <li><Link to={"/thoughts"}>Thoughts</Link></li>
       </ul>
+    </div>);
+  }
+}*/
+
+class Header extends Component {
+  render() {
+    return (
+    <div className="head">
+      <header>
+        <h1>Duy Nguyen</h1>
+        <div className="nav">
+          <ul>
+            <li><Link to={"/about/"}>About</Link></li>
+            <li><Link to={"/projects/"}>Projects</Link></li>
+            <li><Link to={"/thoughts"}>Thoughts</Link></li>
+          </ul>
+        </div>
+      </header>
     </div>);
   }
 }
